f4ab8490e5cccee01c14586908c16d374c4cdaf4
shihab01118 <shahed.shihab.0@gmail.com>
2023-10-17 22:21:10 +0600

    navbar added with required information

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,9 +1,12 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
 
 
 const Root = () => {
     return (
         <div>
-            <h2>This is root</h2>
+            <Navbar />
+            <Outlet />
         </div>
     );
 };
--- src/routes/PublicRoutes.jsx
+++ src/routes/PublicRoutes.jsx
@@ -1,13 +1,40 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layouts/Root";
 import ErrorPage from "../pages/Error/ErrorPage";
+import Home from "../pages/Home/Home";
+import AddProduct from "../pages/AddProduct/AddProduct";
+import MyCart from "../pages/MyCart/MyCart";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
 
 
 const PublicRoutes = createBrowserRouter([
     {
         path: "/",
         element: <Root />,
-        errorElement: <ErrorPage />
+        errorElement: <ErrorPage />, 
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/add_product",
+                element: <AddProduct />
+            },
+            {
+                path: "/my_cart",
+                element: <MyCart />
+            },
+            {
+                path: "/login",
+                element: <Login />
+            },
+            {
+                path: "/register",
+                element: <Register />
+            },
+        ]
     }
 ])
 
