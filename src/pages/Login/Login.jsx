5f819b7aa731f5c897af2fc4cd101982f3c82a72
shihab01118 <shahed.shihab.0@gmail.com>
2023-10-18 14:26:15 +0600

    login page added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,76 @@
-
+import { Link } from "react-router-dom";
+import { FaFacebook, FaGoogle } from "react-icons/fa";
 
 const Login = () => {
-    return (
-        <div>
-            
+  return (
+    <div
+      className="flex justify-center py-10 lg:py-16 bg-cover bg-center"
+      style={{
+        backgroundImage:
+          "url(https://preview.colorlib.com/theme/wed/assets/img/gallery/section_bg1.png.webp)",
+      }}
+    >
+      <div className="relative flex flex-col text-gray-700 bg-white shadow-md w-96 rounded-xl bg-clip-border">
+        <div className="relative grid mx-4 mb-4 -mt-6 overflow-hidden text-white shadow-lg h-28 place-items-center rounded-xl bg-[#ffb300]">
+          <h3 className="block font-sans text-3xl antialiased font-semibold leading-snug tracking-normal text-white">
+            Sign In
+          </h3>
         </div>
-    );
+        <div className="flex flex-col gap-4 p-6">
+          <div className="relative h-11 w-full min-w-[200px]">
+            <input
+              className="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+              placeholder=" "
+            />
+            <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+              Email
+            </label>
+          </div>
+          <div className="relative h-11 w-full min-w-[200px]">
+            <input
+              className="w-full h-full px-3 py-3 font-sans text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+              placeholder=" "
+            />
+            <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+              Password
+            </label>
+          </div>
+        </div>
+        <div className="p-6 pt-0">
+          <button
+            className="block w-full select-none rounded-lg bg-[#ffb300] py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white"
+            type="button"
+            data-ripple-light="true"
+          >
+            Sign In
+          </button>
+          <p className="flex justify-center mt-6 font-sans text-sm antialiased font-light leading-normal text-inherit">
+            Don{"'"}t have an account?
+            <Link to="/register" className="text-[#ffb300] font-semibold">
+              Register
+            </Link>
+          </p>
+          <div>
+            <div className="relative flex py-5 items-center">
+              <div className="flex-grow border-t border-gray-400"></div>
+              <span className="flex-shrink mx-4">Continue With</span>
+              <div className="flex-grow border-t border-gray-400"></div>
+            </div>
+            <div className="grid grid-cols-2 gap-5 mt-3">
+              <button className="btn btn-outline">
+                <FaGoogle></FaGoogle>
+                Google
+              </button>
+              <button className="btn btn-outline">
+                <FaFacebook className="text-blue-600"></FaFacebook>
+                Facebook
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
